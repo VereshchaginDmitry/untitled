bb1014788f1e8fb71f93816c4949e20618e974bf
VereshchaginDmitry <adv.68.1@gmail.com>
2016-09-07 15:08:13 +0300

    Initial commit

---
+++ src/com/example/untitled/untitled.java
@@ -0,0 +1,10 @@
+package com.example.untitled;
+
+/**
+ * Created by Admin on 30.08.2016.
+ */
+public class untitled {
+    public static void main(String[] args) {
+        System.out.println("Hello,Java");
+    }
+}
